ae6b56091af4b38630c9d34bd60604fda6a5091b
pavan bagwe <96296445+pavanb0@users.noreply.github.com>
2022-05-04 21:03:00 +0530

    Create copy image.py

---
+++ copy image.py
@@ -0,0 +1,6 @@
+#############1################Write a program to copy image file
+se=open("C:\\Users\\a2z\\Desktop\\fuf.jpg","rb")
+new=open("C:\\Users\\a2z\\Desktop\\fu#ck.jpg","wb")
+new.write(se.read())
+se.close()
+new.close()
